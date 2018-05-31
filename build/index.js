3941aca0bf12d7c73ecb886849281dff6f0a4dd4
Khaled Osman <khaled.osmaan@gmail.com>
2018-05-31 14:45:16 +0200

    add build dir to gitignore

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.listen(8000, () => console.log("Server running on 8000!"));
-//# sourceMappingURL=index.js.map
