49dc7ce8b99e75f3fda1da431d3dab36e6ddd17c
Khaled Osman <khaled@colorfy.me>
2018-07-05 10:57:11 +0200

    set up linting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
-import * as express from "express"
+import * as express from 'express'
 
 const app = express()
 
-app.listen(8000, () => console.log("Server running on 8000!"))
+app.listen(8000, () => console.log('Server running on 8000!'))
