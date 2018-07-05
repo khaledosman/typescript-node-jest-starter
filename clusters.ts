64d96fe8073318c50f7a75779d0676205ca001df
Khaled Osman <khaled@colorfy.me>
2018-07-05 14:10:22 +0200

    add clustering for scaling/spawning multiple forks of the app

---
+++ clusters.ts
@@ -0,0 +1,23 @@
+// spawns multiple instances of the app
+import * as cluster from 'cluster'
+import * as os from 'os'
+
+const CPUS = os.cpus()
+if (cluster.isMaster) {
+  CPUS.forEach(() => {
+    cluster.fork()
+  })
+  cluster.on('listening', function (worker) {
+    console.log('Cluster %d connected', worker.process.pid)
+  })
+  cluster.on('disconnect', function (worker) {
+    console.log('Cluster %d disconnected', worker.process.pid)
+  })
+  cluster.on('exit', function (worker) {
+    console.log('Cluster %d is dead', worker.process.pid)
+        // Ensuring a new cluster will start if an old one dies
+    cluster.fork()
+  })
+} else {
+  import('./src/index')
+}
