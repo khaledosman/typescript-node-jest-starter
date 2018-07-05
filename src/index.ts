68fc2ede660dfcc356050a2a67d87ab5a4f66c68
Khaled Osman <khaled@colorfy.me>
2018-07-05 14:35:28 +0200

    integrate bunyan for advanced logging

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 import * as compression from 'compression'
 import { config } from 'dotenv'
 import * as express from 'express'
+import * as expressBunyanLogger from 'express-bunyan-logger'
 import * as helmet from 'helmet'
-
+import { log } from './logger'
 config()
 export const app = express()
 
@@ -11,6 +12,15 @@ app.use(compression())
 app.use(helmet())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send({ message: 'Hello world!' }))
+app.use(expressBunyanLogger({name: 'AppName',
+  streams: [{
+    type: 'rotating-file'
+    // level: 'info',                  // loging level
+    // path: './logs/AppName.log',
+    // period: '1d',   // daily rotation
+    // count: 3        // keep 3 back copies
+  }]
+}))
 
-app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}!`))
+app.get('/', (req, res) => res.send({ message: 'Hello world!' }))
+app.listen(process.env.PORT, () => log.info(`Server running on ${process.env.PORT}!`))
---
+++ src/logger.ts
@@ -0,0 +1,10 @@
+import * as bunyan from 'bunyan'
+
+export const log = bunyan.createLogger({
+  name: `AppName`, streams: [{
+    type: 'rotating-file'
+    // path: `./logs/AppName.log`,
+    // period: '1d',   // daily rotation
+    // count: 3        // keep 3 back copies
+  }]
+})
